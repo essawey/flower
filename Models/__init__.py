273cf11cb60d4a9280e173bdd466cdfe00907f0d
essawey <M.Abdelmaged@nu.edu.eg>
2025-01-11 13:09:00 +0200

    added single losses

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,6 +1,5 @@
 from .segmentation_models import DeepLabV3Plus_model
 from .segmentation_models import Segformer_model
-from .segmentation_models import Unet_model
 from .loss import CategoricalCrossEntropyLoss, MultiDiceLoss, CombinedLoss
 from .metric import MeanIoU
 from .trainer import Trainer
